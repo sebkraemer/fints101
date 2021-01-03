1c62fe763036ed2d62809dfb0f7f2cea03693433
Sebastian Krämer <sebkraemer@posteo.org>
2021-01-03 20:37:11 +0100

    visitor now adds pseudo-tags to visited nodes
    This proof of concept should enable html style
    markup easily as a further step.
    
    output: visitChildren result:
    <segmentkopf><segmentkennung> <DT_AN>H</DT_AN>  <DT_AN>I</DT_AN>  <DT_AN>B</DT_AN>  <DT_AN>N</DT_AN>  <DT_AN>K</DT_AN> </segmentkennung> <DEG_SEP>:</DEG_SEP> <segmentnummer> <DT_num>1</DT_num> </segmentnummer> <DEG_SEP>:</DEG_SEP> <segmentversion> <DT_num>1</DT_num> </segmentversion> <DEG_SEP>:</DEG_SEP> <bezugssegment> <DT_num>1</DT_num> </bezugssegment></segmentkopf>

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+const { ParserRuleContext } = require('antlr4');
+const { TerminalNode } = require('antlr4/tree/Tree');
 var http = require('http');
 var fints = require('./fints.js');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   const input = "HIBNK:1:1:1"
   res.write(`input: ${input}\n`);
 
@@ -14,33 +16,45 @@ http.createServer(function (req, res) {
   console.log(treeString);
   res.write(`unmodified parse tree for 'segmentkopf' rule via toStringTree(): ${treeString}\n`);
 
-  // some debug
-  const theChild = treeSegmentkopf.children[1].toString();
-  const testText = treeSegmentkopf.children[1].getText();
-  console.log(testText);
-  const testSymbol = treeSegmentkopf.children[1].getSymbol();
-  const tokentype = testSymbol.type; //why is getType() not defined?
-  console.log(testSymbol);
+  const parserRules = parser.ruleNames; // relate to ParserRuleContext
+  const symbolicNames = parser.symbolicNames; // relate to TerminalNode
+
+  // todo helper function to get rule or parser name as text from index/'type'
 
   class Visitor {
     visitChildren(ctx) {
       if (!ctx) {
         return;
       }
-  
+
+      let tagName = 'unknownTag'
+      if (ctx instanceof ParserRuleContext) {
+        const index = ctx.ruleIndex
+        tagName = parserRules[ctx.ruleIndex];
+      }
+
+      let result = `<${tagName}>`;
+
       if (ctx.children) {
-        return ctx.children.map(child => {
+        ctx.children.forEach(child => {
+          const childText = child.getText();
           if (child.children && child.children.length != 0) {
-            return child.accept(this);
+            const childStr = child.toString();
+            result += child.accept(this);
           } else {
-            return child.getText();
+            if (child instanceof TerminalNode) {
+              const index = child.getSymbol().type; //why is getType() not defined?
+              const symbolName = symbolicNames[index];
+              result += ` <${symbolName}>${childText}</${symbolName}> `;
+            }
           }
         });
       }
+      return result + `</${tagName}>`;
     }
   }
-  
+
   const visitResult = treeSegmentkopf.accept(new Visitor());
 
-  res.end(`visitChildren result: ` + JSON.stringify(visitResult));
+  res.end(`visitChildren result: \n` + visitResult);
 }).listen(3101);
