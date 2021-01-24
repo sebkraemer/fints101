74a2d470625ed483dbacb54110fa48b10224d23a
Sebastian Krämer <sebkraemer@posteo.org>
2021-01-24 23:41:11 +0100

    HTML fetch() with form data and display of parse result
    Without page reloading, a GET request is now sent and the parse result is displayed
    in the DIV field.
    
    This is work in progress, e.g. no error checking yet.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,40 @@
-var http = require('http');
-var fs = require('fs');
-var fints = require('./fints.js');
+const http = require('http');
+const fs = require('fs');
+const querystring = require('querystring');
+const url = require('url');
+const fints = require('./fints.js');
 
 http.createServer(function (req, res) {
+  const reqUrl = url.parse(req.url, true);
   if (req.url === '/style.css') {
+    // send CSS
     fs.readFile('./src/style.css', function (err, data) {
       res.writeHead(200, { "Content-Type": "text/css" });
       res.write(data);
       res.end();
     });
   }
+  else if (req.method === "GET" && reqUrl.pathname === "/parse") {
+    // parse form data and send result html
+    console.log("parse!!")
+    const parseData = reqUrl.query;
+
+    const input = parseData.fintsInput; // e.g. "HIBNK:1:1:1"
+    const parser = fints.getParser(input);
+    const treeSegmentkopf = parser.segmentkopf();
+    const treeString = treeSegmentkopf.toStringTree();
+    console.debug(`unmodified parse tree for 'segmentkopf' rule via toStringTree(): ${treeString}<br>\n`);
+
+    const ruleNames = parser.ruleNames; // relates to ParserRuleContext
+    const symbolicNames = parser.symbolicNames; // relates to TerminalNode
+    const visitResult = treeSegmentkopf.accept(new fints.HtmlVisitor(ruleNames, symbolicNames));
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(visitResult);
+    res.end();
+  }
   else {
+    // send main page
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(`<!doctype html>\n<html lang="en"> <head>
       <title>FinTS 101</title>
@@ -20,7 +44,7 @@ http.createServer(function (req, res) {
 
       <label for="fintsInputArea">FinTS message</label>
       <form id="fintsForm">
-      <textarea id="fintsInputArea" name="fintsInputArea" rows="10" cols="80" padding="3" autofocus>HIBNK:1:1:1</textarea>
+      <textarea id="fintsInputArea" name="fintsInput" rows="10" cols="80" padding="3" autofocus>HIBNK:1:1:1</textarea>
       <p>
       <button type="submit" name="submitButton" value="Submit">Submit</button>
       <button type="reset" name="resetButton" value="Reset">Reset</button>
@@ -35,25 +59,21 @@ http.createServer(function (req, res) {
         const form = document.getElementById("fintsForm");
         const sb = form.elements["submitButton"];
         sb.addEventListener("click", event => {
-          document.getElementById("resultDiv").innerHTML = form.elements["fintsInputArea"].value;
-          document.getElementById("resultStatus").innerHTML = "clicked";
+          // document.getElementById("resultDiv").innerHTML = form.elements["fintsInputArea"].value;
+          // document.getElementById("resultStatus").innerHTML = "clicked";
           event.preventDefault();
+
+          const parseInput = form.elements["fintsInputArea"].value;
+          const request = new Request("/parse?fintsInput=" + encodeURIComponent(parseInput) );
+          fetch(request)
+          .then( responseData => { return responseData.text(); } )
+          .then( parseResult => {
+            let resultDiv = document.getElementById("resultDiv");
+            resultDiv.innerHTML = parseResult;
+          });
         });
       </script>
       `);
-    const input = "HIBNK:1:1:1"
-    const parser = fints.getParser(input);
-    const voc = parser.vocabulary;
-    const treeSegmentkopf = parser.segmentkopf();
-    const treeString = treeSegmentkopf.toStringTree();
-
-    //res.write(`unmodified parse tree for 'segmentkopf' rule via toStringTree(): ${treeString}<br>\n`);
-
-    const ruleNames = parser.ruleNames; // relates to ParserRuleContext
-    const symbolicNames = parser.symbolicNames; // relates to TerminalNode
-    const visitResult = treeSegmentkopf.accept(new fints.HtmlVisitor(ruleNames, symbolicNames));
-
-    //res.write(`visitChildren result: \n` + visitResult);
     res.end("<br>\n</body></html>");
   }
 }).listen(3101);
